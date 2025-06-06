0e6d8df8f01248b6a79a523c93d01bc058fcbcd3
pranav0121 <pranav0121@users.noreply.github.com>
2025-06-06 19:07:43 +0530

    trash

--- chatbot_cloud/tests/test_database_setup.py
+++
@@ -1,6 +0,0 @@
-class TestDatabaseSetup:
-    def test_database_connection(self):
-        from config_fixed import Config
-        assert Config.SQLALCHEMY_DATABASE_URI is not None
-        assert "mssql+pyodbc://" in Config.SQLALCHEMY_DATABASE_URI
-        assert "trusted_connection=yes" in Config.SQLALCHEMY_DATABASE_URI

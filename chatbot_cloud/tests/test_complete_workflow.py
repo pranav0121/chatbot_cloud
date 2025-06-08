15bb2295897f935174cdf5b6a8f6dad965b3ed0a
pranav0121 <pranav0121@users.noreply.github.com>
2025-06-08 23:50:43 +0530

    del prv

--- chatbot_cloud/tests/test_complete_workflow.py
+++
@@ -1,3 +0,0 @@
-class TestCompleteWorkflow:
-    def test_complete_workflow(self):
-        assert True  # Replace with actual workflow tests to ensure all components work together seamlessly.
--- chatbot_cloud/tests/test_config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    DB_SERVER = 'your_database_server'
-    DB_DATABASE = 'your_database_name'
-    SQLALCHEMY_DATABASE_URI = f'mssql+pyodbc://{DB_SERVER}/{DB_DATABASE}?driver=your_driver&trusted_connection=yes'
-
-def test_config():
-    assert Config.DB_SERVER == 'your_database_server'
-    assert Config.DB_DATABASE == 'your_database_name'
-    assert Config.SQLALCHEMY_DATABASE_URI == f'mssql+pyodbc://{Config.DB_SERVER}/{Config.DB_DATABASE}?driver=your_driver&trusted_connection=yes'

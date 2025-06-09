925ea414643719b57f03b6a5520ec3778b3e0142
pranav0121 <pranav0121@users.noreply.github.com>
2025-06-09 14:50:17 +0530

    fresh

--- chatbot_cloud/tests/test_app_initialization.py
+++
@@ -1,6 +0,0 @@
-class TestAppInitialization:
-    def test_application_initialization(self):
-        from config_fixed import Config
-        assert Config.DB_SERVER is not None
-        assert Config.DB_DATABASE is not None
-        assert isinstance(Config.SQLALCHEMY_DATABASE_URI, str)
